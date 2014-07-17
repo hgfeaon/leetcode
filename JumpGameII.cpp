d172ce5966d8740d70b992e6ff61acc6c4a42050
hgfeaon <hgfeaon@qq.com>
2014-07-17 16:38:02 +0800

    update JumpGameII

--- JumpGameII.cpp
+++ JumpGameII.cpp
@@ -68,6 +68,39 @@ public:
     }
 };
 
+class SolutionBetter {
+public:
+    int jump(int A[], int n) {
+        if (A == nullptr || n <= 0) {
+            return -1;
+        } else if (n == 1) {
+            return 0;
+        }
+
+        int last_pos, this_pos;
+        int i;
+        int result;
+
+        last_pos = 0;
+        this_pos = 0;
+        result = 0;
+        for (i = 0; this_pos < n - 1; ++i) {
+            if (i > this_pos) {
+                return -1;
+            }
+            if (i + A[i] > this_pos) {
+                if (i > last_pos) {
+                    last_pos = this_pos;
+                    ++result;
+                }
+                this_pos = i + A[i];
+            }
+        }
+
+        return result + 1;
+    }
+};
+
 int main() {
     int A[] = {1, 1, 2, 1, 1};
     int len = sizeof(A) / sizeof(int);
